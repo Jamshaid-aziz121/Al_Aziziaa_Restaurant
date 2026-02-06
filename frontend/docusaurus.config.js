b73db4196f8ef9dcdc400a504b6448dc7a202efe
jamshaid <jamshaid.aziz121@gmail.com>
2026-02-06 16:27:39 +0500

    base url updated

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://jamshaid-aziz121.github.io/Al_Aziziaa_Restaurant/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'Al_Aziziaa_Restaurant',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
